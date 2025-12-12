60f1b8ae932019e1b4bd714de0a134fd2b7ae614
shototoy <julecyrusarabit@sksu.edu.ph>
2025-12-12 13:14:42 +0800

    finished cabrera

--- index.php
+++ index.php
@@ -4,22 +4,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Alshera Seravanez Montano - BS CPE | Portfolio</title>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-      media="all"
-      onload="this.media='all'"
-    />
-    <noscript>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-      />
-    </noscript>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Outfit:wght@300;400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
@@ -304,7 +288,7 @@
 
     <footer role="contentinfo">
       <p>
-        © 2024 <span data-content="fullName">Alshera Seravanez Montano</span>.
+        © 2025 <span data-content="fullName">Alshera Seravanez Montano</span>.
         All rights reserved.
       </p>
     </footer>
